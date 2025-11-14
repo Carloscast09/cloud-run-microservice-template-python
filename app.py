7e82cccf6f733b99273f43c8dcd48c5dd7c844e0
Carlos Cabrera Castrejón <162522369+Carloscast09@users.noreply.github.com>
2025-11-13 19:36:16 -0600

    Change return message in app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, World!"
+    return "Holaaaaaa, Charly desde la clase de Computo en la nube :)"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
